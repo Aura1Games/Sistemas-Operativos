c7ae20006f3d78b390d5828d245216a4354b23d1
Elian Gutierrez <u192168195ser@gmail.com>
2025-11-14 11:06:11 -0300

    Add ejemploLogs.sh with log structure and entries

---
+++ ejemploLogs.sh
@@ -0,0 +1,25 @@
+#Estructura_de_log:<tipo>_<cosa>_<detalle>_<fecha>
+#Ruta_/var/log/proyectoAura.log
+#Comando_de_busqueda_awk
+-respaldo-volumen-bd-2025-11-07
+-respaldo-carpetaPersonal-full-2025-11-08
+-respaldo-carpetaPersonal-diferencal-2025-11-08
+-respaldo-carpetaPersonal-diferencal-2025-11-08
+-grupo-rootYyo-agregado-2025-11-08
+-usuario-yoyRoot-creado con carpeta de trabajo /home/yoyRoot y en grupo wheel-2025-11-08
+-usuario-yoyRoot-eliminado-2025-11-08
+-grupo-modificado-nuevo grupo principal de otroma caca-2025-11-08
+-grupo-modificado-nuevo grupo secundario de otroma cosete021-2025-11-08
+-usuario-modificado-nueva carpeta personal de otroma /home/cosete021-2025-11-08
+-usuario-otroma-nueva contraseña-2025-11-08
+-respaldo-carpetaPersonal-full-2025-11-09
+-respaldo-volumen-bd-2025-11-09
+-respaldo-volumen-bd-2025-11-09
+-respaldo-volumen-bd hecho por el usuario-2025-11-09
+-docker-aplicacionWebBd-levantada correctamente-2025-11-10
+-docker-aplicacionWebBd-levantada correctamente-2025-11-10
+-docker-aplicacionWebBd-levantada correctamente-2025-11-10
+-docker-aplicacionWebBd-levantada correctamente-2025-11-10
+-docker-aplicacionWebBd-apagada correctamente-2025-11-10
+-docker-contenedorWeb-levantado correctamente-2025-11-10
+-docker-aplicacionWebBd-apagada correctamente-2025-11-10
